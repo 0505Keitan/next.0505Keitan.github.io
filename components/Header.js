a52f95d72db1151c65d5e755597e3039aa0e062e
0505Keitan <0505Keitan@gmail.com>
2021-03-12 13:01:39 +0900

    update: About link

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Profile() {
       <a href="/" className={styles.title}>0505Keitan</a>
       <nav className={styles.nav}>
         <ul className={styles.nav_list}>
-          <li><a href="https://www.notion.so/0505keitan/0505Keitan-ebc28d56ab2f4baeb896165924054214">About</a></li>
+          <li><a href="https://scrapbox.io/0505Keitan/index">About</a></li>
           <li><a href="https://portfolio.0505keitan.now.sh/">Portfolio</a></li>
         </ul>
       </nav>
