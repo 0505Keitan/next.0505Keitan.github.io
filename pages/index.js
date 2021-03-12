fa52d24ac9e683ec0610360c1143ce0a053dcae8
0505Keitan <0505Keitan@gmail.com>
2021-03-12 12:36:14 +0900

    update: title

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <Header />
       <div className={styles.container}>
         <Head>
-          <title>Create Next App</title>
+          <title>next.0505Keitan.com</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
 
