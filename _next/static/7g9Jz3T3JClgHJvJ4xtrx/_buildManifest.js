0c8ca7cbc46ba6ba7e8e9394ead468e74d4c3436
0505Keitan <0505Keitan@users.noreply.github.com>
2021-03-12 03:37:05 +0000

    deploy: fa52d24ac9e683ec0610360c1143ce0a053dcae8

--- _next/static/7g9Jz3T3JClgHJvJ4xtrx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fa9a7754c.2ba921b10bdb2242881e.js","static\u002Fchunks\u002Fpages\u002Findex-66bef11e6e70cd62d85e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-fd2fb679ee0369d3b482.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
